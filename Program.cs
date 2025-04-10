d92efcac2da1a2b1f154fc72bd490917545015c3
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-10 10:49:44 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AirlineReservationConsoleSystem_CSharpProject3
+{
+    internal class Program
+    {
+        public static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Airline Reservation System");
+        }
+        static void Main(string[] args)
+        {
+            DisplayWelcomeMessage();
+        }
+    }
+}
